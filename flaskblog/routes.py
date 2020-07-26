58c7c95a209c749b624249bc3be60daa962bf37c
adityasunny1189 <adityapathak1189@gmail.com>
2020-07-26 13:18:57 +0530

    adding developer section

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -13,9 +13,10 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
+	total_user = db.session.query(User).count()
 	page = request.args.get('page', 1, type = int)
 	posts = Post.query.order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
-	return render_template('index.html', blog_posts = posts)
+	return render_template('index.html', blog_posts = posts, total_user = total_user)
 
 
 @app.route('/about')
@@ -75,6 +76,7 @@ def save_picture(form_picture):
 @app.route('/account', methods = ['GET', 'POST'])
 @login_required
 def account():
+	total_user = db.session.query(User).count()
 	form = UpdateProfile()
 	if form.validate_on_submit():
 		if form.picture.data:
@@ -89,11 +91,12 @@ def account():
 		form.username.data = current_user.username
 		form.email.data = current_user.email
 	image_file = url_for('static', filename = 'profile_pics/' + current_user.avatar)
-	return render_template('account.html', title = 'profile', image_file = image_file, form = form)
+	return render_template('account.html', title = 'profile', image_file = image_file, form = form, total_user = total_user)
 
 @app.route('/post/new', methods = ['GET', 'POST'])
 @login_required
 def new_post():
+	total_user = db.session.query(User).count()
 	form = PostForm()
 	if form.validate_on_submit():
 		post = Post(title = form.title.data, content = form.content.data, author = current_user)
@@ -101,17 +104,19 @@ def new_post():
 		db.session.commit()
 		flash('Your Post has been created', 'success')
 		return redirect(url_for('home'))
-	return render_template('create_post.html', title = 'create post', form = form, legend = 'create post')
+	return render_template('create_post.html', title = 'create post', form = form, legend = 'create post', total_user = total_user)
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
+	total_user = db.session.query(User).count()
 	post = Post.query.get_or_404(post_id)
-	return render_template('post.html', title = post.title, post = post)
+	return render_template('post.html', title = post.title, post = post, total_user = total_user)
 
 @app.route('/post/<int:post_id>/update', methods = ['GET', 'POST'])
 @login_required
 def update_post(post_id):
 	post = Post.query.get_or_404(post_id)
+	total_user = db.session.query(User).count()
 	if post.author != current_user:
 		abort(403)
 	form = PostForm()
@@ -124,7 +129,7 @@ def update_post(post_id):
 	elif request.method == 'GET':
 		form.title.data = post.title
 		form.content.data = post.content
-	return render_template('create_post.html', title = 'Update post', form = form, legend = 'Update Post')
+	return render_template('create_post.html', title = 'Update post', form = form, legend = 'Update Post', total_user = total_user)
 
 @app.route('/post/<int:post_id>/delete', methods = ['POST'])
 @login_required
@@ -139,10 +144,11 @@ def delete_post(post_id):
 
 @app.route('/user/<string:username>')
 def user_post(username):
+	total_user = db.session.query(User).count()
 	user = User.query.filter_by(username = username).first_or_404()
 	page = request.args.get('page', 1, type = int)
 	posts = Post.query.filter_by(author = user).order_by(Post.date_posted.desc()).paginate(page = page, per_page = 5)
-	return render_template('user_post.html', blog_posts = posts, user = user)
+	return render_template('user_post.html', blog_posts = posts, user = user, total_user = total_user)
 
 
 @app.route('/chat', methods = ['GET', 'POST'])
@@ -202,12 +208,15 @@ def reset_token(token):
 # Custom error pages
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html', title = "404")
+	total_user = db.session.query(User).count()
+	return render_template('404.html', title = "404", total_user = total_user)
 
 @app.errorhandler(403)
 def page_not_found(e):
-	return render_template('403.html', title = "403")
+	total_user = db.session.query(User).count()
+	return render_template('403.html', title = "403", total_user = total_user)
 
 @app.errorhandler(500)
 def page_not_found(e):
-	return render_template('500.html', title = "500")
+	total_user = db.session.query(User).count()
+	return render_template('500.html', title = "500", total_user = total_user)
